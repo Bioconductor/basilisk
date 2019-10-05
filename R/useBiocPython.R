7e41f502abcaf97dc151d4a58d7c26679a7dfe1e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-10-04 23:56:45 -0700

    Added set-up function based on reticulate.

---
+++ R/useBiocPython.R
@@ -0,0 +1,23 @@
+#' Use the Bioconductor Python
+#'
+#' Use \pkg{reticulate} to set up the Bioconductor-owned instance of Python provided by \pkg{jormungandR}.
+#'
+#' @return
+#' A string containing the path to the \pkg{jormungandR} Python executable, invisibly.
+#'
+#' @author Aaron Lun
+#'
+#' @details
+#' This function is intended for use within other Bioconductor packages.
+#' It uses \code{\link{use_python}} to register a consistent Python version that is isolated from the system installation.
+#' 
+#' @examples
+#' useBiocPython()
+#' os <- reticulate::import("os")
+#' os$listdir()
+#'
+#' @export
+#' @importFrom reticulate use_python
+useBiocPython <- function() {
+    use_python(system.file("inst", "python", "bin", "python3", package="jormungandR", mustWork=TRUE), required=TRUE)
+}
