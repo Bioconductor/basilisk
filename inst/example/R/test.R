d33594fbdc88de23dd76a9c64e5a3426b43f8265
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-10-23 22:36:58 -0700

    Updated son.of.basilisk test function.

--- inst/example/R/test.R
+++ inst/example/R/test.R
@@ -9,17 +9,22 @@
 #' test()
 #' @export
 #' @importFrom reticulate import
-#' @importFrom basilisk callVirtualEnv
+#' @importFrom basilisk basiliskStart
+#' @importFrom parallel stopCluster clusterCall
 test <- function() {
-    pandas.names <- callVirtualEnv("env1", pkgname="son.of.basilisk", FUN=function() {
+    cl <- basiliskStart("env1", pkgname="son.of.basilisk")
+    pandas.names <- clusterCall(cl, function() { 
         X <- reticulate::import("pandas")
         names(X) 
     })
+    stopCluster(cl)
 
-    sklearn.names <- callVirtualEnv("env2", pkgname="son.of.basilisk", FUN=function() {
+    cl <- basiliskStart("env2", pkgname="son.of.basilisk")
+    sklearn.names <- clusterCall(cl, function() { 
         X <- reticulate::import("sklearn.ensemble")
         names(X)
     })
+    stopCluster(cl)
 
     list(pandas=pandas.names, sklearn=sklearn.names)
 }
