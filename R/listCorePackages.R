97bc12afe1aaf798529b08887ed8cee1305c83d4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-07 21:28:24 -0800

    Avoid factors, minor doc edit.

--- R/listCorePackages.R
+++ R/listCorePackages.R
@@ -7,9 +7,9 @@
 #' These are usually infrastructure packages that are required by many other Python packages,
 #' so maintaining a core installation avoids redundancy and reduces the installation footprint.
 #'
-#' Please contact the \pkg{basilisk} maintainers if you feel that a Python package should be added to this list.
+#' Please contact the \pkg{basilisk} maintainers if you feel that a Python package should be added or an existing version bumped.
 #' This requires that (i) multiple Bioconductor packages depend on the proposed Python package and
-#' (ii) the proposed package does not have any version conflicts with existing core packages.
+#' (ii) the proposed package does not have any version conflicts with other core packages.
 #'
 #' @author Aaron Lun
 #'
@@ -22,7 +22,7 @@
 listCorePackages <- function() {
     pkgs <- readLines(system.file("core_list", package="basilisk"))
     names <- .full2pkg(pkgs)
-    data.frame(full=pkgs, name=names)
+    data.frame(full=pkgs, name=names, stringsAsFactors=FALSE)
 }
 
 .full2pkg <- function(packages) {
