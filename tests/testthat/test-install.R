407a9b8c0761749e5be98281afeae03a7b493221
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-09 18:19:37 -0800

    Extended installation tests, use basilisk instead of system2.

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -9,14 +9,16 @@ dir.create(client.dir)
 
 test.py <- file.path(basilisk.dir, 'bin', 'python3')
 Sys.setenv(BASILISK_TEST_PYTHON=test.py)
+Sys.setenv(BASILISK_TEST_CORE=TRUE)
 
 test_that("setupVirtualEnv refuses to work without all specified versions", {
     expect_error(setupVirtualEnv("thingo", "pandas", pkgpath=client.dir), "must be explicitly specified")
     expect_error(setupVirtualEnv("thingo", "pandas==0.25.1", pkgpath=client.dir), "need to list dependency")
+    expect_error(setupVirtualEnv("thingo", c("pandas==0.24.1", "pandas==0.25.1"), pkgpath=client.dir), "redundant listing")
 })
 
 test_that("setupVirtualEnv switches to a core installation when possible", {
-    incoming <- system2(test.py, c("-m", "pip", "freeze"), stdout=TRUE)
+    incoming <- basilisk:::.basilisk_freeze(test.py)
     expect_false(any(grepl("numpy==", incoming)))
 
     unlink(file.path(client.dir, "basilisk"), recursive=TRUE)
@@ -24,7 +26,7 @@ test_that("setupVirtualEnv switches to a core installation when possible", {
 
     core <- readLines(system.file("core_list", package="basilisk"))
     expected.numpy <- core[grep("numpy==", core)]
-    incoming <- system2(test.py, c("-m", "pip", "freeze"), stdout=TRUE)
+    incoming <- basilisk:::.basilisk_freeze(test.py)
     observed.numpy <- incoming[grep("numpy==", incoming)]
     expect_identical(expected.numpy, observed.numpy)
 })
@@ -33,14 +35,27 @@ test_that("setupVirtualEnv overrides an incompatible core installation", {
     unlink(file.path(client.dir, "basilisk"), recursive=TRUE)
     expect_error(setupVirtualEnv("thingo", c("numpy==1.15.1"), pkgpath=client.dir), NA)
 
-    incoming <- system2(test.py, c("-m", "pip", "freeze"), stdout=TRUE)
+    incoming <- basilisk:::.basilisk_freeze(test.py)
     expect_true("numpy==1.17.3" %in% incoming)
 
     env.py <- file.path(client.dir, 'basilisk', 'thingo', 'bin', 'python3')
-    incoming <- system2(env.py, c("-m", "pip", "freeze"), stdout=TRUE)
+    incoming <- basilisk:::.basilisk_freeze(env.py)
     expect_true("numpy==1.15.1" %in% incoming)
+})
+
+test_that("setupVirtualEnv allows core packages to have unspecified versions", {
+    unlink(file.path(client.dir, "basilisk"), recursive=TRUE)
+    system2(test.py, c("-m", "pip", "uninstall", "-y", "numpy"))
+
+    incoming <- basilisk:::.basilisk_freeze(test.py)
+    expect_false("numpy==1.17.3" %in% incoming)
+
+    expect_error(setupVirtualEnv("thingo", "numpy", pkgpath=client.dir), NA)
+    incoming <- basilisk:::.basilisk_freeze(test.py)
+    expect_true("numpy==1.17.3" %in% incoming)
 })
 
 unlink(client.dir, recursive=TRUE)
 unlink(basilisk.dir, recursive=TRUE)
 Sys.unsetenv("BASILISK_TEST_PYTHON")
+Sys.unsetenv("BASILISK_TEST_CORE")
