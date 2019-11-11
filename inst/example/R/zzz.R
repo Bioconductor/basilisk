e3a14bd21deee09961f654d1a921c10241b2ea9e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-11 00:21:50 -0800

    Update setup example in son.of.basilisk.

--- inst/example/R/zzz.R
+++ inst/example/R/zzz.R
@@ -1,9 +1,9 @@
 .onLoad <- function(libname, pkgname) {
     basilisk::setupVirtualEnv("env1", 
         c("pandas==0.25.1", "python-dateutil==2.8.1", "pytz==2019.3"),
-        pkgpath=system.file(package=pkgname))
+        pkgname=pkgname)
 
     basilisk::setupVirtualEnv("env2", 
         c("scikit-learn==0.21.0", "joblib==0.14.0"), 
-        pkgpath=system.file(package=pkgname))
+        pkgname=pkgname)
 }
