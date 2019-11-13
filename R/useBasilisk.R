70992adccee036b30ca198bb6f5952c16fe14717
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-12 23:22:03 -0800

    Clarified docs about ana/miniconda origins.

--- R/useBasilisk.R
+++ R/useBasilisk.R
@@ -1,6 +1,7 @@
 #' Use \pkg{basilisk}'s Python instance
 #'
 #' Use \pkg{reticulate} to set up the Bioconductor-owned instance of Python provided by \pkg{basilisk}.
+#' This function is primarily intended for developer intended for use within other Bioconductor packages.
 #'
 #' @return
 #' A string containing the path to the \pkg{basilisk} Python executable, invisibly.
@@ -8,9 +9,15 @@
 #' @author Aaron Lun
 #'
 #' @details
-#' This function is intended for use within other Bioconductor packages.
-#' It uses \code{\link{use_python}} to register a consistent Python version that is isolated from the system installation.
+#' This uses \code{\link{use_python}} to register a consistent Python version that is isolated from the system installation.
+#' The \pkg{basilisk}-owned Python is installed using miniconda 4.7.10 (\url{https://docs.conda.io/en/latest/miniconda.html}),
+#' with additional packages being lazily installed to eventually reach a full Anaconda installation.
+#'
+#' Developers should generally not use this function for anything other than testing.
+#' Clients of \pkg{basilisk} should interact with its Python instance via \code{\link{basiliskStart}} and related functions,
+#' while installation of additional packages should be performed via \code{\link{setupVirtualEnv}}.
 #' 
+#' @seealso
 #' @examples
 #' useBasilisk()
 #' os <- reticulate::import("os")
@@ -25,7 +32,11 @@ useBasilisk <- function() {
         on.exit(Sys.setenv(RETICULATE_PYTHON=old))
     }
 
-    py.cmd <- .get_py_cmd(system.file(.mc_dir, package="basilisk", mustWork=TRUE))
+    py.cmd <- .get_basilisk_py()
     use_python(py.cmd, required=TRUE)
     py.cmd
+}
+
+.get_basilisk_py <- function() {
+    .get_py_cmd(system.file(.mc_dir, package="basilisk", mustWork=TRUE))
 }
