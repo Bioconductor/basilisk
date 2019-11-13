bf36afce094174b84c47ac2cb6ff3b7a5bc11502
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-13 09:50:54 -0800

    Added tests for lack of permissions during installation.

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -9,11 +9,19 @@ Sys.setenv(WORKON_HOME=client.dir)
 basilisk.dir <- "install-test-basilisk" 
 basilisk:::.minstaller(basilisk.dir)
 
+Sys.setenv(BASILISK_TEST_MINICONDA=basilisk.dir)
 test.py <- basilisk:::.get_py_cmd(basilisk.dir)
-Sys.setenv(BASILISK_TEST_PYTHON=test.py)
 
 Sys.setenv(BASILISK_TEST_COMMON=normalizePath(client.dir)) # normalization required for correct symlinks.
 reticulate::virtualenv_create(basilisk:::.common_env, python=test.py)
+
+uninstaller <- function(pkg="numpy") {
+    system2(test.py, c("-m", "pip", "uninstall", "-y", pkg))
+}
+
+cleanenv <- function(envname="thingo") {
+    unlink(file.path(client.dir, envname), recursive=TRUE)
+}
 
 #############################
 
@@ -23,23 +31,26 @@ test_that("setupVirtualEnv refuses to work without all specified versions", {
     expect_error(setupVirtualEnv("thingo", c(test.pandas, old.pandas)), "redundant listing")
 })
 
-uninstaller <- function() {
-    system2(test.py, c("-m", "pip", "uninstall", "-y", "numpy"))
-}
-
 test_that("setupVirtualEnv switches to a core installation when possible", {
     uninstaller()
     incoming <- basilisk:::.basilisk_freeze(test.py)
     expect_false(any(grepl("numpy==", incoming)))
 
-    unlink(file.path(client.dir, "thingo"), recursive=TRUE)
+    cleanenv()
     expect_error(setupVirtualEnv("thingo", c(test.pandas, test.pandas.deps)), NA)
 
+    # Numpy is indeed installed in the core.
     core <- listCorePackages()$full
     expected.numpy <- core[grep("numpy==", core)]
     incoming <- basilisk:::.basilisk_freeze(test.py)
     observed.numpy <- incoming[grep("numpy==", incoming)]
     expect_identical(expected.numpy, observed.numpy)
+
+    # Pandas is only installed in the virtual environment.
+    expect_false(any(grepl("pandas==", incoming)))
+    env.py <- basilisk:::.get_py_cmd(file.path(client.dir, "thingo"))
+    incoming <- basilisk:::.basilisk_freeze(env.py)
+    expect_true(test.pandas %in% incoming)
 })
 
 all.core <- listCorePackages()
@@ -47,7 +58,7 @@ test.numpy <- all.core$full[all.core$name=="numpy"]
 old.numpy <- "numpy==1.15.1"
 
 test_that("setupVirtualEnv overrides an incompatible core installation", {
-    unlink(file.path(client.dir, "thingo"), recursive=TRUE)
+    cleanenv()
     expect_error(setupVirtualEnv("thingo", old.numpy), NA)
 
     incoming <- basilisk:::.basilisk_freeze(test.py)
@@ -59,7 +70,7 @@ test_that("setupVirtualEnv overrides an incompatible core installation", {
 })
 
 test_that("setupVirtualEnv allows core packages to have unspecified versions", {
-    unlink(file.path(client.dir, "thingo"), recursive=TRUE)
+    cleanenv()
     uninstaller()
 
     incoming <- basilisk:::.basilisk_freeze(test.py)
@@ -91,10 +102,9 @@ test_that("setupVirtualEnv skips virtual environment creation for pure core pack
     expect_false(identical(s1, ""))
     expect_identical(s1, s2)
 
-    FUN <- function(envir1, envir2, test.py, client.dir) {
+    FUN <- function(envir1, envir2, client.dir) {
         library(basilisk)
         library(testthat)
-        Sys.setenv(BASILISK_TEST_PYTHON=test.py)
         Sys.setenv(WORKON_HOME=client.dir)
 
         proc <- basiliskStart(envir1)
@@ -118,12 +128,47 @@ test_that("setupVirtualEnv skips virtual environment creation for pure core pack
     }
 
     library(callr)
-    expect_true(r(FUN, args=list(envir1="thingo1", envir2="thingo2", test.py=test.py, client.dir=client.dir), show=TRUE))
+    expect_true(r(FUN, args=list(envir1="thingo1", envir2="thingo2", client.dir=client.dir), show=TRUE))
+})
+
+test_that("setupVirtualEnv handles lack of permissions correctly", {
+    cleanenv()
+    setupVirtualEnv("thingo", c(test.pandas, test.pandas.deps))
+
+    # Setting up is still okay without write permissions for the client directory.
+    Sys.chmod(file.path(client.dir, "thingo"), "0555")
+    expect_error(setupVirtualEnv("thingo", c(test.pandas, test.pandas.deps)), NA)
+
+    # Recovery fails without write permissions for the client directory.
+    Sys.chmod(file.path(client.dir, "thingo"), "0755")
+    env.py <- basilisk:::.get_py_cmd(file.path(client.dir, "thingo"))
+    system2(env.py, c("-m", "pip", "uninstall", "-y", "pandas"))
+
+    Sys.chmod(file.path(client.dir, "thingo"), "0555")
+    expect_error(setupVirtualEnv("thingo", c(test.pandas, test.pandas.deps)), "invalid Python environment")
+
+    # Recovery succeeds with write permissions for the client directory.
+    Sys.chmod(file.path(client.dir, "thingo"), "0755")
+    expect_error(setupVirtualEnv("thingo", c(test.pandas, test.pandas.deps)), NA)
+
+    # Core installation diverts to virtual environment without write permissions to basilisk.
+    uninstaller()
+    cleanenv()
+
+    Sys.chmod(basilisk.dir, "0555")
+    setupVirtualEnv("thingo", c(test.pandas, test.pandas.deps))
+
+    incoming <- basilisk:::.basilisk_freeze(test.py)
+    expect_false(any(grepl("numpy==", incoming)))
+    incoming <- basilisk:::.basilisk_freeze(env.py)
+    expect_true(any(grepl("numpy==", incoming)))
+
+    Sys.chmod(basilisk.dir, "0755") # to enable deletion.
 })
 
 #############################
 
 unlink(client.dir, recursive=TRUE)
 unlink(basilisk.dir, recursive=TRUE)
-Sys.unsetenv("BASILISK_TEST_PYTHON")
+Sys.unsetenv("BASILISK_TEST_MINICONDA")
 Sys.unsetenv("BASILISK_TEST_COMMON")
