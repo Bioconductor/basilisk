40181ea842dfbe585290c0aa39e6f0bc7f39672e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-20 14:56:08 -0800

    Switch to using Anaconda to pull down all core packages.

--- R/zzz.R
+++ R/zzz.R
@@ -21,34 +21,34 @@
 .minstaller <- function(dest_path, testing=FALSE) {
     # Stripped from https://github.com/hafen/rminiconda
     # To be replaced if rminiconda gets into CRAN, or if reticulate offers its own solution.
-    version <- "4.7.12"
-    base_url <- "https://repo.anaconda.com/miniconda/"
+    version <- "2019.10"
+    base_url <- "https://repo.anaconda.com/archive"
     os <- .detect_os()
 
     if (os %in% c("win64", "win32")) {
         arch <- if (os=="win64") "x86_64" else "x86"
-        inst_file <- sprintf("Miniconda3-%s-latest-Windows-%s.exe", version, arch)
+        inst_file <- sprintf("Anaconda3-%s-Windows-%s.exe", version, arch)
         tmploc <- .expedient_download(paste0(base_url, inst_file))
-        inst_args <- sprintf(" /InstallationType=JustMe /RegisterPython=0 /S /D=%s", dest_path)
+        inst_args <- sprintf(" /InstallationType=JustMe /RegisterPython=0 /S /D=%s", normalizePath(dest_path, mustWork=FALSE))
         system2(tmploc, inst_args)
 
     } else {
         sysname <- if (os=="macosx") "MacOSX" else "Linux"
-        inst_file <- sprintf("Miniconda3-%s-%s-x86_64.sh", version, sysname)
+        inst_file <- sprintf("Anaconda3-%s-%s-x86_64.sh", version, sysname)
 
         if (testing) {
             # FOR INTERNAL TESTING ONLY, avoid re-downloading and 
             # re-installing miniconda everytime we update the R package.
             dest_path2 <- file.path(path.expand("~/"), ".miniconda")
             if (!file.exists(dest_path2)) {
-                tmploc <- .expedient_download(paste0(base_url, inst_file))
+                tmploc <- .expedient_download(file.path(base_url, inst_file))
                 inst_args <- sprintf(" %s -b -p %s", tmploc, dest_path2)
                 system2("bash", inst_args)
             }
 
             file.symlink(dest_path2, dest_path)
         } else {
-            tmploc <- .expedient_download(paste0(base_url, inst_file))
+            tmploc <- .expedient_download(file.path(base_url, inst_file))
             inst_args <- sprintf(" %s -b -p %s", tmploc, dest_path)
             system2("bash", inst_args)
         }
