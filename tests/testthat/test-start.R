10a2eef421577fb1ea0762447bf47be48dd21bd1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-12-10 00:18:41 -0800

    Updated tests to check for proper persist= interactions.

--- tests/testthat/test-start.R
+++ tests/testthat/test-start.R
@@ -11,12 +11,12 @@ old.version <- sub(".*==", "", old.pandas)
 ###########################################################
 
 test_that("basilisk directly loads Python when possible", {
-    FUN <- function(version, envir) {
+    FUN <- function(version, envir, persist=FALSE) {
         library(basilisk)
         library(testthat)
         Sys.setenv(WORKON_HOME="whee")
 
-        proc <- basiliskStart(envir)
+        proc <- basiliskStart(envir, persist=persist)
         expect_true(is.environment(proc))
 
         test.version <- basiliskRun(proc, fun=function() {
@@ -42,7 +42,11 @@ test_that("basilisk directly loads Python when possible", {
     expect_true(r(FUN, args=list(version=new.version, envir="my_package_A")))
     expect_true(r(FUN, args=list(version=old.version, envir="my_package_B")))
 
-    # Respects persistence.
+    # Unaffected by persist=TRUE.
+    expect_true(r(FUN, args=list(version=new.version, envir="my_package_A", persist=TRUE)))
+    expect_true(r(FUN, args=list(version=old.version, envir="my_package_B", persist=TRUE)))
+
+    # Respects persistence of variables.
     expect_true(r(persistence_check, args=list(version=new.version, envir="my_package_A")))
     expect_true(r(persistence_check, args=list(version=old.version, envir="my_package_B")))
 })
@@ -57,9 +61,9 @@ test_that("basilisk forks when possible", { # ... though on windows, this just u
     expect_true(r(preloaded_check, args=list(version=new.version, envir="my_package_A")))
     expect_true(r(preloaded_check, args=list(version=old.version, envir="my_package_B")))
 
-    # Respects persistence.
-    expect_true(r(persistence_check, args=list(version=new.version, envir="my_package_A", global=FALSE)))
-    expect_true(r(persistence_check, args=list(version=old.version, envir="my_package_B", global=FALSE)))
+    # Respects persistence of variables.
+    expect_true(r(persistence_check, args=list(version=new.version, envir="my_package_A", shared=FALSE)))
+    expect_true(r(persistence_check, args=list(version=old.version, envir="my_package_B", shared=FALSE)))
 })
 
 ###########################################################
@@ -72,7 +76,47 @@ test_that("basilisk uses sockets as a fallback", {
     expect_true(r(preloaded_check, args=list(version=new.version, envir="my_package_A", fork=FALSE)))
     expect_true(r(preloaded_check, args=list(version=old.version, envir="my_package_B", fork=FALSE)))
 
-    # Respects persistence.
-    expect_true(r(persistence_check, args=list(version=new.version, envir="my_package_A", global=FALSE, fork=FALSE)))
-    expect_true(r(persistence_check, args=list(version=old.version, envir="my_package_B", global=FALSE, fork=FALSE)))
+    # Respects persistence of variables.
+    expect_true(r(persistence_check, args=list(version=new.version, envir="my_package_A", shared=FALSE, fork=FALSE)))
+    expect_true(r(persistence_check, args=list(version=old.version, envir="my_package_B", shared=FALSE, fork=FALSE)))
+})
+
+###########################################################
+
+test_that("basilisk works with persistent processes", {
+    expect_true(r(process_check, args=list(version=new.version, envir="my_package_A", persist=TRUE, fork=FALSE)))
+    expect_true(r(process_check, args=list(version=old.version, envir="my_package_B", persist=TRUE, fork=FALSE)))
+
+    out <- r(function(version) {
+        library(basilisk)
+        library(testthat)
+        Sys.setenv(WORKON_HOME="whee")
+
+        envname <- "my_package_A"
+        pkgname <- NULL
+        proc <- basiliskStart(envname, shared=FALSE)
+        old.pid <- parallel::clusterCall(basilisk:::.get_persist(envname, pkgname), Sys.getpid)[[1]]
+
+        # Same process is re-used.
+        proc2 <- basiliskStart(envname, shared=FALSE)
+        new.pid <- parallel::clusterCall(basilisk:::.get_persist(envname, pkgname), Sys.getpid)[[1]]
+        expect_identical(old.pid, new.pid)
+
+        test.version <- basiliskRun(proc2, fun=function() {
+            reticulate::import("pandas")$`__version__`
+        })
+        expect_identical(version, test.version)
+
+        # Reboots persistent process if it's been closed.
+        actual_proc <- basilisk:::.get_persist(envname, pkgname)
+        parallel::stopCluster(actual_proc)
+        basilisk:::.set_persist(envname, pkgname, actual_proc)
+        expect_error(parallel::clusterCall(basilisk:::.get_persist(envname, pkgname), Sys.getpid))
+
+        reproc <- basiliskStart(envname, shared=FALSE)
+        expect_error(parallel::clusterCall(basilisk:::.get_persist(envname, pkgname), Sys.getpid), NA)
+
+        TRUE
+    }, args=list(version=new.version))
+    expect_true(out)
 })
