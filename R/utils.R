2728a04f26816d72703c51095059dfb8f4e40da4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-24 00:32:51 -0800

    More robust retrieval of client envs.

--- R/utils.R
+++ R/utils.R
@@ -17,19 +17,21 @@
 
 #' @importFrom utils packageVersion
 #' @importFrom basilisk.utils isWindows isMacOSX getExternalDir
-.choose_env_dir <- function(pkgname, mustWork=FALSE) {
+.choose_env_dir <- function(pkgname, assume.installed=FALSE) {
     if (isWindows() || isMacOSX()) {
         vdir <- file.path(getExternalDir(), 
             paste0(pkgname, "-", packageVersion(pkgname)))
     } else {
-        # As this is run in configure, system.file() will not
-        # work, as pkgname isn't even installled yet!
-        vdir <- file.path(.libPaths()[1], pkgname)
+        if (assume.installed) {
+            # This is more robust than .libPaths(), which may change
+            # between *basilisk* installation and client installation;
+            # system.file() should still pull out the correct dir.
+            vdir <- system.file(package=pkgname)
+        } else {
+            # As this is run in configure, system.file() will not
+            # work, as pkgname isn't even installled yet!
+            vdir <- file.path(.libPaths()[1], pkgname)
+        }
     }
-
-    vdir <- file.path(vdir, .env_dir)
-    if (mustWork && !file.exists(vdir)) {
-        stop("basilisk environment directory does not exist")
-    }
-    vdir
+    file.path(vdir, .env_dir)
 }
