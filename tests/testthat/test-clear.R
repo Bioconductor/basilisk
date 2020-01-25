11560467e1d8a84a6442da3cc5b1e55dcf0b05de
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-24 22:37:18 -0800

    Added tests for lazy installation, wiping external directory.

---
+++ tests/testthat/test-clear.R
@@ -0,0 +1,29 @@
+# Tests the basilisk.utils::clearExternalDir() utility.
+# We put these tests in here rather than in basilisk.utils because 
+# these functions depend on basilisk being installed.
+# library(testthat); library(basilisk); source("test-clear.R")
+
+test_that("clearing out the external directory works as expected", {
+    tmp <- tempfile()
+    dir.create(tmp)
+
+    version <- as.character(packageVersion("basilisk"))
+    placeholder <- file.path(tmp, paste0(version, "000"))
+    dir.create(placeholder, showWarnings=FALSE)
+    expect_true(file.exists(placeholder))
+
+    version2 <- "0.0.0"
+    placeholder2 <- file.path(tmp, version2)
+    dir.create(placeholder2, showWarnings=FALSE)
+    expect_true(file.exists(placeholder2))
+
+    old <- Sys.getenv("BASILISK_EXTERNAL_DIR")
+    Sys.setenv(BASILISK_EXTERNAL_DIR=tmp)
+    expect_identical(basilisk.utils::getExternalDir(), file.path(tmp, version))
+
+    basilisk.utils::clearExternalDir()
+    expect_false(file.exists(placeholder))
+    expect_true(file.exists(placeholder2))
+
+    Sys.setenv(BASILISK_EXTERNAL_DIR=old)
+})
---
+++ tests/testthat/test-lazy.R
@@ -0,0 +1,33 @@
+# This tests the lazy installation of an Anaconda installation via installAnaconda().
+# We put this here as it depends on an existing installation of Anaconda.
+# library(testthat); library(basilisk); source("test-lazy.R")
+
+test_that("lazy Anaconda installation works as expected", {
+    skip_on_os("windows") # avoid problems with long paths on Windows.
+
+    tmp <- tempfile()
+    dir.create(tmp)
+
+    old <- Sys.getenv("BASILISK_USE_SYSTEM_DIR")
+    Sys.unsetenv("BASILISK_USE_SYSTEM_DIR")
+    old.2 <- Sys.getenv("BASILISK_EXTERNAL_DIR")
+    Sys.setenv(BASILISK_EXTERNAL_DIR=tmp)
+
+    version <- as.character(packageVersion("basilisk"))
+    placeholder <- file.path(tmp, paste0(version, "000"))
+    dir.create(placeholder, showWarnings=FALSE)
+    expect_true(file.exists(placeholder))
+
+    expect_true(basilisk.utils::installAnaconda())
+    target <- file.path(tmp, packageVersion("basilisk"), "anaconda")
+    expect_true(file.exists(target))
+    expect_false(file.exists(placeholder))
+
+    # Skips the next evaluation, as we've already created it.
+    expect_false(basilisk.utils::installAnaconda())
+    expect_true(file.exists(target))
+
+    Sys.setenv(BASILISK_EXTERNAL_DIR=old.2)
+    Sys.setenv(BASILISK_USE_SYSTEM_DIR=old)
+})
+
